bbfde94881385d4218b09fa316e204ce68110b91
Harizo07 <hei.harizo@gmail.com>
2023-11-17 20:38:30 +0300

    succes connection with database

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class Main {
+    public static void main(String[] args) {
+        String jdbcUrl = System.getenv("DB_URL");
+        String username = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password)) {
+            System.out.println("Connexion à la base de données établie avec succès!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
